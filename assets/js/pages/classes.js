a65e43013715f21b8445e76d354409d3ff6484dc
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 07:13:39 +0700

    Add classes.js for class page structure

---
+++ assets/js/pages/classes.js
@@ -0,0 +1,8 @@
+window.SFMS_PAGES.classes = function () {
+  document.getElementById("page").innerHTML = `
+    <div class="card">
+      <h2>Kelas</h2>
+      <div class="empty">Modul kelas akan kita isi pada tahap berikutnya.</div>
+    </div>
+  `;
+};
