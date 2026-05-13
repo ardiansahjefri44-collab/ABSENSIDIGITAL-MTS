bf9fb15f20a047ae727afe61652af04f01d04538
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 08:44:12 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+window.SFMS_CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxYFLfh0TXg6kuQYxFwDPaa5ZcC-MH7POur_DhxrNC15_dxh0_5uYZHRE6PpwH5nuce/exec",
+  SESSION_KEY: "sfms_session_v3"
+};
