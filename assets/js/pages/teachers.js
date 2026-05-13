9163e698b51772f0c1b02a742a6bed8ef74edcbd
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 07:15:18 +0700

    Create teachers.js

---
+++ assets/js/pages/teachers.js
@@ -0,0 +1,8 @@
+window.SFMS_PAGES.teachers = function () {
+  document.getElementById("page").innerHTML = `
+    <div class="card">
+      <h2>Guru & Mapel</h2>
+      <div class="empty">Modul guru dan mapel akan kita isi pada tahap berikutnya.</div>
+    </div>
+  `;
+};
